50b7f31c5c686d3996afb11332c16b315802d9bc
ciscoadiz <ciscoadiz@gmail.com>
2022-04-06 18:42:58 +0100

    refactoring: cambios en los test GeneratorShould

--- src/test/java/GeneratorShould.java
+++ src/test/java/GeneratorShould.java
@@ -1,39 +1,37 @@
 import com.leanmind.ciscoadiz.bizzbuzz.FizzBuzz;
+import com.leanmind.ciscoadiz.bizzbuzz.Generator;
 import com.leanmind.ciscoadiz.bizzbuzz.Text;
 import org.junit.Assert;
 import org.junit.Test;
 
-import java.util.Arrays;
-import java.util.List;
 
-
-public class FizzBuzzShould {
+public class GeneratorShould {
     @Test
     public void buildFizzBuzzFirstPlaceShouldBeOne() {
         Text expected = new Text("1");
-        FizzBuzz fizzBuzz = new FizzBuzz();
-        Assert.assertEquals(expected, fizzBuzz.buildFizzBuzz().toArray()[0]);
+        var generator = new Generator();
+        Assert.assertEquals(expected, generator.make(1));
     }
 
     @Test
     public void buildFizzBuzzThirdPlaceShouldBeFizz() {
         Text expected = new Text("Fizz");
-        FizzBuzz fizzBuzz = new FizzBuzz();
-        Assert.assertEquals(expected, fizzBuzz.buildFizzBuzz().toArray()[2]);
+        var generator = new Generator();
+        Assert.assertEquals(expected, generator.make(3));
     }
 
     @Test
     public void buildFizzBuzzFifthPlaceShouldBeBuzz() {
         Text expected = new Text("Buzz");
-        FizzBuzz fizzBuzz = new FizzBuzz();
-        Assert.assertEquals(expected, fizzBuzz.buildFizzBuzz().toArray()[4]);
+        var generator = new Generator();
+        Assert.assertEquals(expected, generator.make(5));
     }
 
     @Test
     public void buildFizzBuzzFifteenthPlaceShouldBeFizzBuzz() {
         Text expected = new Text("FizzBuzz");
-        FizzBuzz fizzBuzz = new FizzBuzz();
+        var generator = new Generator();
 
-        Assert.assertEquals(expected, fizzBuzz.buildFizzBuzz().toArray()[14]);
+        Assert.assertEquals(expected, generator.make(15));
     }
 }
